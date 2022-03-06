9ac6621188660cf2c890f7255d09e7188333e3c9
DESKTOP-J0GN1PA\Amir Javaid <41790096+AmirJavaidSwe@users.noreply.github.com>
2022-03-06 09:22:16 +0500

    fix

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -26,6 +26,7 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         Paginator::useBootstrapFour();
+        \Schema::defaultStringLength(191);
         //
     }
 }
